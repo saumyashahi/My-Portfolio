06ec1cb44caa8a5a6a6d57f17f4e52f5c16e83e2
Saumya Shahi <saumyashahi05@gmail.com>
2025-06-22 06:16:19 +0530

    chore: integrate new components and global styles

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import Navbar from './components/Navbar';
 import Hero from './components/Hero/Hero';
 import Experience from './components/Experience/Experience';
 import Projects from './components/Projects/Projects';
+import CustomCursor from './components/CustomCursor/CustomCursor';
 import './App.css';
 
 function App() {
   return (
     <>
+      <CustomCursor />
       <Navbar />
       <Hero />
       <Experience />
